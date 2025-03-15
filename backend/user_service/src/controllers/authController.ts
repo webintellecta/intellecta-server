dccb11d8e213193720c59fad34b719896086d609
hari-1018 <harikrishnanclt2001@gmail.com>
2025-03-15 11:46:55 +0530

    modified functions

--- backend/user_service/src/controllers/authController.ts
+++ backend/user_service/src/controllers/authController.ts
@@ -1,6 +1,19 @@
-import { Request, Response } from "express";
-import { changePasswordService, loginUserService, logOutUserService, registerUser } from "../service/authService";
+import { NextFunction, Request, Response } from "express";
+import {
+  changePasswordService,
+  loginUserService,
+  logOutUserService,
+  registerUser,
+} from "../service/authService";
 import CustomError from "../utils/customErrorHandler";
+import User from "../models/userModel";
+import jwt from "jsonwebtoken";
+import nodemailer from "nodemailer";
+
+import dotenv from "dotenv";
+import { hashPassword } from "../utils/passwordHash";
+
+dotenv.config();
 
 //registeration
 export const userRegistration = async (req: Request, res: Response) => {
@@ -8,13 +21,12 @@ export const userRegistration = async (req: Request, res: Response) => {
   if (!data) {
     throw new CustomError("registration failed", 404);
   }
-  return res
-    .status(200)
-    .json({ success: true, message: "user registered successfully" });
+  return res.status(200).json({
+    success: true,
+    message: "user registered successfully",
+    data: data,
+  });
 };
-
-
-
 
 //login
 export const userLogin = async (req: Request, res: Response) => {
@@ -23,15 +35,92 @@ export const userLogin = async (req: Request, res: Response) => {
 };
 
 //logout
-export const userLogout = async (req:Request, res:Response) => {
-  const userData = await logOutUserService(req.body, res)
-  return res.status(200).json({message:"user logged out", data:userData})
-}
+export const userLogout = async (req: Request, res: Response) => {
+  const userData = await logOutUserService(req.body, res);
+  return res.status(200).json({ message: "user logged out", data: userData });
+};
 
 //changePassword
-export const userChangePassword = async (req:Request, res:Response) => {
-  const userId = req.user
-  const changePsswdData = await changePasswordService(userId , req.body)
-  return res.status(200).json({message:"password changed"})
-}
+export const userChangePassword = async (req: Request, res: Response) => {
+  const userId = req.user;
+  if (!userId) {
+    throw new CustomError("user not found, please login", 404);
+  }
+  const changePsswdData = await changePasswordService(userId, req.body);
+  return res.status(200).json({ message: "password changed" });
+};
 
+//forgot-password & reset-password
+export const forgotPassword = async (req: Request, res: Response) => {
+  const userId = req.params.id;
+  const currentUser = await User.findById(userId).select("password email");
+  if (!currentUser?.email || !currentUser?.password) {
+    throw new CustomError("user not found", 404);
+  }
+  console.log("email", currentUser.email);
+
+  const secret = process.env.TOKEN_SECRET + currentUser.password;
+  if (!secret) {
+    throw new CustomError("token not found", 404);
+  }
+  console.log("this controller is working");
+  const token = jwt.sign({ id: currentUser._id }, secret, { expiresIn: "1h" });
+  const resetURL = `http://localhost:4586/api/user/resetPassword?id=${userId}&token=${token}`;
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "intellectademo@gmail.com",
+      pass: "iqgg fgtp hsfe jkwk",
+    },
+  });
+
+  const mailOptions = {
+    to: currentUser.email,
+    from: "intellectademo@gmail.com",
+    subject: "Password Reset Request",
+    text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+    Please click on the following link, or paste this into your browser to complete the process:\n\n
+    ${resetURL}\n\n
+    If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+  };
+  await transporter.sendMail(mailOptions);
+  return res.status(200).json({ message: "Password reset link sent" });
+};
+
+// =========================================
+
+export const resetPassword = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id, token } = req.query;
+  const { password } = req.body;
+  if (!id || !token) {
+    throw new CustomError("data not found", 404);
+  }
+  const strToken = token.toString();
+  const user = await User.findById(id).select("password");
+  console.log("user", user);
+  if (!user) {
+    return res.status(400).json({ message: "User not exists!" });
+  }
+  const secret = process.env.TOKEN_SECRET + user.password;
+  const verify = jwt.verify(strToken, secret);
+  const encryptedPassword = await hashPassword(password);
+  await User.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        password: encryptedPassword,
+      },
+    }
+  );
+
+  await user.save();
+
+  res.status(200).json({ message: "Password has been reset" });
+};

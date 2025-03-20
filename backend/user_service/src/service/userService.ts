710c24181365c54d766cfb5b16ae8035df714e1b
hari-1018 <harikrishnanclt2001@gmail.com>
2025-03-20 13:22:35 +0530

    fix(profile): resolve profile picture display

--- backend/user_service/src/service/userService.ts
+++ backend/user_service/src/service/userService.ts
@@ -1,16 +1,21 @@
 import { JwtPayload } from "jsonwebtoken"
 import User from "../models/userModel"
 import CustomError from "../utils/customErrorHandler"
-import { uploadToS3 } from "../middleware/profilePicUploader";
+import { uploadToS3, generatePresignedUrl } from "../middleware/profilePicUploader";
 
+//Get User by ID
 export const getUserByIdService = async(userId:string | undefined | JwtPayload) =>{
     const specificUser = await User.findById(userId)
     if(!specificUser){
         throw new CustomError("user not found", 404)
     }
+    console.log("object", specificUser.profilePic);
+    if (specificUser.profilePic !== null && specificUser.profilePic !== undefined) {
+        specificUser.profilePic = await generatePresignedUrl(specificUser.profilePic);
+      }
     return{
         message:"user data fetched",
-        user:specificUser
+        user: specificUser
     }
 }
 
@@ -34,6 +39,21 @@ export const profilePictureService = async (userId: string, file: Express.Multer
     }
 
     return fileUrl;
+};
+
+//Edit User
+export const userEditService = async (id: string, data: { name?: string; email?: string; age?: number; phone?: string }) => {
+    const { name, email, age, phone } = data;
+    const user = await User.findByIdAndUpdate(id, { name, email, age, phone }, { new: true });
+
+    if (!user) {
+        throw new CustomError("User not found, Try Again", 404);
+    }
+
+    return {
+        message: "User details updated successfully",
+        user
+    };
 };
 
 
--- backend/user_service/src/utils/jwt.ts
+++ backend/user_service/src/utils/jwt.ts
@@ -5,6 +5,7 @@ import CustomError from "./customErrorHandler";
 
 export const generateToken = (userId: string | unknown): string => {
   if (!process.env.TOKEN_SECRET) {
+    console.log("within if", process.env.TOKEN_SECRET)
     throw new CustomError(
       "token secret is not defined in the environment variables",
       404
@@ -16,9 +17,10 @@ export const generateToken = (userId: string | unknown): string => {
 };
 
 export const generateRefreshToken = (userId: string | unknown): string => {
+  console.log("generating token", process.env.REFRESH_TOKEN_SECRET);
   if (!process.env.REFRESH_TOKEN_SECRET) {
     throw new CustomError(
-      "token secret is not defined in the environment variables",
+      "refresh token is not defined in the environment variables",
       404
     );
   }

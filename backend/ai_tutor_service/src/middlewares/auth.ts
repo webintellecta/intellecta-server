edd16a699c90b77344d77796ceabbe561bde9f4a
hari-1018 <harikrishnanclt2001@gmail.com>
2025-03-25 15:51:27 +0530

    fix(app.ts): fix issues in cors

--- backend/ai_tutor_service/src/middlewares/auth.ts
+++ backend/ai_tutor_service/src/middlewares/auth.ts
@@ -15,22 +15,27 @@ export const authMiddleware = (
   next: NextFunction
 ): void => {
   try {
+    console.log("mid try");
     const tokenFromCookie = req.cookies?.token;
     const authHeader = req.headers.authorization;
     const tokenFromHeader = authHeader?.startsWith('Bearer ') 
       ? authHeader.substring(7) 
       : null;
     const token = tokenFromCookie || tokenFromHeader; 
+    console.log(" token: ", req.cookies);
     if (!token) {
       res.status(401).json({ message: "Access Denied. No token provided." });
       return;
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-    req.user = decoded as DecodedUser; 
+    console.log("decoded", decoded);  
+    req.user = decoded as DecodedUser;
+    console.log("req user", req.user); 
 
     return next();
   } catch (error) {
+    console.log("mid catch");
      res.status(400).json({ message: "Invalid Token" });
   }
 };

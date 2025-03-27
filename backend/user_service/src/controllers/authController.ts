90a2b23d0b8b75c70388220b0a9ec443b7071f92
silpa4251 <silpadas4251@gmail.com>
2025-03-27 10:23:12 +0530

    fix(authController): updated the publishToQueue code

--- backend/user_service/src/controllers/authController.ts
+++ backend/user_service/src/controllers/authController.ts
@@ -1,20 +1,13 @@
 import { NextFunction, Request, Response } from "express";
-import {
-  changePasswordService,
-  loginUserService,
-  logOutUserService,
-  registerUser,
-} from "../service/authService";
+import {changePasswordService,loginUserService,logOutUserService,registerUser,} from "../service/authService";
 import CustomError from "../utils/customErrorHandler";
 import User from "../models/userModel";
 import jwt from "jsonwebtoken";
 import nodemailer from "nodemailer";
-
 import dotenv from "dotenv";
 import { hashPassword } from "../utils/passwordHash";
 import { generateToken, verifyToken } from "../utils/jwt";
 import { googleAuthentication } from "../service/authService";
-// import { publishToQueue } from "../utils/rabbitmq";
 
 dotenv.config();
 
@@ -34,20 +27,12 @@ export const userRegistration = async (req: Request, res: Response) => {
 //login
 export const userLogin = async (req: Request, res: Response) => {
   const loginData = await loginUserService(req.body, res);
-
-  const queuePayload = {
-    ...loginData,
-    age: loginData.user.age
-  };
-
   return res.status(200).json({ message: "user logged in", data: loginData });
 };
 
 //google login
 export const googleAuth = async (req: Request, res: Response) => {
   const response = await googleAuthentication(req.body, res);
-  // await publishToQueue("user_fetched", response);
-  console.log(response)
   res.status(200).json({ status: "success", message: "Successfully logged in with Google", data: response });
 };
 
@@ -68,8 +53,6 @@ export const userChangePassword = async (req: Request, res: Response) => {
   return res.status(200).json({ message: "password changed" });
 };
 
-
-
 //forgot-password & reset-password
 export const forgotPassword = async (req: Request, res: Response) => {
   const userId = req.params.id;
@@ -82,7 +65,6 @@ export const forgotPassword = async (req: Request, res: Response) => {
   if (!secret) {
     throw new CustomError("token not found", 404);
   }
-  console.log("this controller is working");
   const token = jwt.sign({ id: currentUser._id }, secret, { expiresIn: "1h" });
   const resetURL = `http://localhost:4586/api/user/resetPassword?id=${userId}&token=${token}`;
 
@@ -107,8 +89,6 @@ export const forgotPassword = async (req: Request, res: Response) => {
   return res.status(200).json({ message: "Password reset link sent" });
 };
 
-// =========================================
-
 export const resetPassword = async (
   req: Request,
   res: Response,
@@ -121,7 +101,6 @@ export const resetPassword = async (
   }
   const strToken = token.toString();
   const user = await User.findById(id).select("password");
-  console.log("user", user);
   if (!user) {
     return res.status(400).json({ message: "User not exists!" });
   }
@@ -140,13 +119,10 @@ export const resetPassword = async (
   );
 
   await user.save();
-
   res.status(200).json({ message: "Password has been reset" });
 };
 
-// ===============================================================
 //refresh token to access token
-
 export const refreshTokeToAccessToken = async (req: Request, res: Response) => {
   const refreshToken = req.cookies.refreshToken;
   if (!refreshToken) {
@@ -166,7 +142,5 @@ export const refreshTokeToAccessToken = async (req: Request, res: Response) => {
   if(!accessToken){
     throw new CustomError("access token generation failed", 404)
   }
-
-
   return res.status(200).json({ message: "Access token generated", accessToken })
 };

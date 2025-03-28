bcaa0bc1e714d6fa4c4794f802d46eef622eb0dd
hari-1018 <harikrishnanclt2001@gmail.com>
2025-03-28 16:46:55 +0530

    fix(assessmentController): get the token from cookies

--- backend/ai_tutor_service/src/controllers/assessmentController.ts
+++ backend/ai_tutor_service/src/controllers/assessmentController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { evaluateAssessmentService, getAssessmentQuesService } from "../services/assessmentQuesService";
+import jwt from "jsonwebtoken";
 
 interface AuthRequest extends Request {
     user?: { _id: string };
@@ -19,9 +20,21 @@ export const getAssessmentQuestions = async (req: AuthRequest, res: Response ): 
 };
 
 export const evaluateAssessment = async( req: Request, res: Response) => {
-  const { assessmentResult } = await evaluateAssessmentService(req.body);
+  const token = req.cookies.token;
+  if (!token) {
+    throw new Error("Not authenticated, please login");
+  }
+  const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as any;
+  const userId = decoded._id;
+  if (!userId) {
+    throw new Error("Invalid user");
+  }
+  console.log("extracting userId: ", userId);
+
+  const { assessmentResult } = await evaluateAssessmentService(userId, req.body);
   res.status(200).json({
     message: "Assessment evaluated successfully",
     assessmentResult
   })
 };
+

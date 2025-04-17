36d1375577ae2e77cc9aea5f8c579b57299ac3e4
silpa4251 <silpadas4251@gmail.com>
2025-04-17 16:24:27 +0530

    feat(controllers): added lesson completion tracking and progress displaying

--- backend/content_service/src/routes/courseRoutes.ts
+++ backend/content_service/src/routes/courseRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { asyncErrorHandler } from "../middlewares/asyncErrorHandler";
 import { authMiddleware } from "../middlewares/auth"
-import { fetchLessonQuiz, generateCourseQuizzesService, getAllCourses, getAllCoursesBySubject, getCourseWithLessons, getFilteredCourses, getLessonById, markLessonAsComplete, searchCourses } from "../controller/courseController";
+import { fetchLessonQuiz, generateCourseQuizzesService, getAllCourses, getAllCoursesBySubject, getCourseWithLessons, getFilteredCourses, getLessonById, searchCourses } from "../controller/courseController";
 
 const courseRouter = express.Router();
 
@@ -11,7 +11,7 @@ courseRouter.get("/", asyncErrorHandler( getAllCourses));
 courseRouter.get("/:subject/filter", asyncErrorHandler( getFilteredCourses ));
 courseRouter.get("/:courseId", asyncErrorHandler( getCourseWithLessons));
 courseRouter.get("/lessons/:lessonId", asyncErrorHandler( getLessonById));
-courseRouter.post("/lessons/:lessonId/complete", authMiddleware, asyncErrorHandler(markLessonAsComplete));
+// courseRouter.post("/lessons/:lessonId/complete", authMiddleware, asyncErrorHandler(markLessonAsComplete));
 courseRouter.get("/subject/:subject",authMiddleware ,asyncErrorHandler( getAllCoursesBySubject));
 courseRouter.post("/generate-quiz", asyncErrorHandler(generateCourseQuizzesService));
 courseRouter.get("/fetch-quiz/:courseId", asyncErrorHandler(fetchLessonQuiz));

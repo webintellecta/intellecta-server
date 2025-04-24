160290a7254968675a05b65a67a7ddf725ab47b0
fariskt <fariskt00@gmail.com>
2025-04-24 10:19:54 +0530

    added edit course and lesson

--- backend/content_service/src/routes/lessonRoutes.ts
+++ backend/content_service/src/routes/lessonRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { addLesson } from "../controller/lessonController";
+import { addLesson, editLesson } from "../controller/lessonController";
 import { upload } from "../middlewares/upload";
 import { asyncErrorHandler } from "../middlewares/asyncErrorHandler";
 
 const lessonRouter = express.Router();
 
 lessonRouter.post("/", upload.single("video"), asyncErrorHandler( addLesson ));
+lessonRouter.put("/editLesson/:lessonId", upload.single("video"), asyncErrorHandler( editLesson ));
 
 export default lessonRouter;
